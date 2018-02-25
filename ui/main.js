32d266418203f3301cdf512ffcde986a829d2473
muhammedsahad <36570913+muhammedsahad@users.noreply.github.com>
2018-02-25 15:11:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,5 +7,5 @@ element.innerHTML='THIS IS MY FIRST WEB APPLICATION AND MY NAME IS MUHAMMED SAHA
 
 var img = document.getElementById('madi');
 img.onclick = function () {
-     img.style.marginRight = '200px';
+     var interval = setinterval(moveRight, 50)
 };
