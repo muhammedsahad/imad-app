b229ae233ec514c6234688b34fddde9bb5ded2ed
muhammedsahad <36570913+muhammedsahad@users.noreply.github.com>
2018-02-25 16:49:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,3 +16,10 @@ img.onclick = function() {
 };
 
 
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+     counter = counter + 1;
+     var span = document.getElementById('count');
+     span.innerHTML = counter.toString();
+}
