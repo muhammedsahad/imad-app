10658f30fc00a4255d26151e6aa9aafb91a8f64d
muhammedsahad <36570913+muhammedsahad@users.noreply.github.com>
2018-02-25 17:15:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,7 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML='THIS IS MY FIRST WEB APPLICATION AND MY NAME IS MUHAMMED SAHAD BIN ABDUL SAMAD';
-
-
-
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-     marginLeft = marginLeft + 10;
-     img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
-};
-
-
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function () {
-     counter = counter + 1;
-     var span = document.getElementById('count');
-     span.innerHTML = counter.toString();
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
