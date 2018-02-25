3a9469645036416f20e1431296bb65abf66688ec
muhammedsahad <36570913+muhammedsahad@users.noreply.github.com>
2018-02-25 12:28:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
-element.innerHTML='I am goin to make this world a better place';
+element.innerHTML='THIS IS MY FIRST WEB APPLICATION AND MY NAME IS MUHAMMED SAHAD BIN ABDUL SAMAD';
